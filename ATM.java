dffd37b5fb0efc6386ce9fd24b429ab02ca5b1c0
Manish Kharbade <86099059+mkharbade@users.noreply.github.com>
2022-11-08 08:24:27 +0530

    First Commit

---
+++ ATM.java
@@ -0,0 +1,20 @@
+package com.project;
+
+import java.io.IOException;
+
+public class ATM extends OptionMenu {
+
+	public static void main(String[] args) {
+		OptionMenu optionMenu = new OptionMenu();
+
+		try {
+
+			optionMenu.getLogin();
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+
+	}
+
+}
